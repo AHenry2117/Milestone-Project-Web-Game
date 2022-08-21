0244f4b0bd4e0e63787160aa17d9860aba18fee9
AHenry2117 <Aehenry2117@gmail.com>
2022-08-21 14:13:04 -0700

    added random functions and adjusted css style

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 
-let userNums = document.querySelectorAll("#start");
-let winningNums = document.querySelectoryAll("#reveal");
+let userNumsList = document.querySelectorAll(".userBall");
+let winningNumsList = document.querySelectoryAll(".winningBall");
+let buttonStart = document.querySelectorAll(".start");
+let buttonReveal = document.querySelectorAll(".reveal");
+let index = 0;
 
 //Return Random integer 1-50
+function displayUser (){
+    userNumsList.forEach(ball =>{
+        ball.innerHTML = random_user_num (); 
+    })
+}
+function displayWinning () {
+    winningNumsList.forEach(wball => {
+        wball.innerHTML = random_winning_num();
+    })
+}
 function userNums(){
     return Math.floor(Math.random()*50)+1;
 }
@@ -10,6 +23,10 @@ function userNums(){
 function winningNums(){
     return Math.floor(Math.random()*50)+1;
 }
-
+buttonReveal.addEventListener('click', () => {
+    let Us
+})
 
 //Button click Event Listener
+
+document.getElementById("start").addEventListener("click",my function);
