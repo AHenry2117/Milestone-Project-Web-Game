7cddc269b598f7e38403ddcb294a2dace1005e51
AHenry2117 <Aehenry2117@gmail.com>
2022-08-23 21:34:15 -0700

    added button functions and refresh button

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 
-let userNumsList = document.querySelectorAll(".userBall");
-let winningNumsList = document.querySelectoryAll(".winningBall");
-let buttonStart = document.querySelectorAll(".start");
-let buttonReveal = document.querySelectorAll(".reveal");
-let index = 0;
+let userNums = document.querySelectorAll(".uball");
+let winningNums = document.querySelectorAll(".wball");
+let userButton = document.querySelector("#start");
+let revealButton = document.querySelector("#reveal")
+let refreshButton = document.querySelector("#refresh")
 
+//Winning/Lose Variables
+
+
+//For Each Loop User & Winning numbers
+function displayUserNums(){
+    userNums.forEach(uball => {
+        uball.innerHTML = random_nums()
+    })
+}
+function revealWinningNums(){
+    winningNums.forEach(wball => {
+        wball.innerHTML = random_nums()
+    })
+}
 //Return Random integer 1-50
-function displayUser (){
-    userNumsList.forEach(ball =>{
-        ball.innerHTML = random_user_num (); 
-    })
+function random_nums(){
+   return Math.floor((Math.random()*50)+1);
 }
-function displayWinning () {
-    winningNumsList.forEach(wball => {
-        wball.innerHTML = random_winning_num();
-    })
+function refreshPage(){
+    window.location.reload();
 }
-function userNums(){
-    return Math.floor(Math.random()*50)+1;
-}
+//Event Listener Buttons
+userButton.addEventListener("click", displayUserNums);
+revealButton.addEventListener("click", revealWinningNums);
+refreshButton.addEventListener("click", refreshPage);z
 
-function winningNums(){
-    return Math.floor(Math.random()*50)+1;
-}
-buttonReveal.addEventListener('click', () => {
-    let Us
-})
-
-//Button click Event Listener
-
-document.getElementById("start").addEventListener("click",my function);
+//If, Else for winner/loser
